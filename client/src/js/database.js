c1fb34f1b669e332cb5290653d27868c85d25ad4
Shao <shaotang.yen@gmail.com>
2022-01-11 11:39:47 +1100

    pushing to heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// A method that accepts some content and adds it to the database
 export const putDb = async (id, content) => {
   //console.log('putDb(): Add content to the database');
   // Create a connection to the database database and version we want to use.
@@ -25,11 +25,11 @@ export const putDb = async (id, content) => {
   const request = store.put({id, content: content });
   // Get confirmation of the request.
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  //console.log('🚀 - data saved to the database', result);
 
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// A method that gets all the content from the database
 export const getDb = async () => { 
   //console.log('getDb(): Getting content from the database');
   // Create a connection to the database database and version we want to use.
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -25,7 +22,8 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'PWA Text Editor'
+        title: 'PWA Text Editor',
+        favicon: "./favicon.ico",
       }),
 
       // Injects our custom service worker
@@ -75,7 +73,6 @@ module.exports = () => {
             },
           },
         },
-
       ],
     },
   };
